6ccc60fccb3396a6793c2838e72300fdff91ebd1
Juvaly <juvaly@gmail.com>
2014-01-01 22:07:17 +0200

    loalization sweep

--- Classy.DotNet.Mvc/ViewModels/Security/LoginViewModel.cs
+++ Classy.DotNet.Mvc/ViewModels/Security/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace Classy.DotNet.Mvc.ViewModels.Security
 {
     public class LoginViewModel
     {
+        [Display(Name="Login_Email")]
         public string Email { get; set; }
+        [Display(Name = "Login_Password")]
         public string Password { get; set; }
+        [Display(Name = "Login_RememberMe")]
         public bool RememberMe { get; set; }
         public string RedirectUrl { get; set; }
     }
